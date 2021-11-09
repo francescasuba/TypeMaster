100eae2ea8005edd8d8f254c6d0d5e7ff86e874a
Ben Leach <benleach@pm.me>
2021-11-09 16:40:22 -0500

    fixes

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 var temp = document.querySelector(".time");
-    var words = document.querySelector(".words");
-    var button = document.querySelector(".nes-btn is-primary");
-    var scoreDiv = document.querySelector(".score");
-    var timerDiv = document.querySelector(".time");
-    var points = 0;
-    var spans;
-    var typed;
-    var seconds = 60;
-
+var words = document.getElementById(".words");
+var levelOneBtn = document.getElementById("level-one-btn");
+var scoreDiv = document.querySelector(".score");
+var timerDiv = document.querySelector(".time");
+var points = 0;
+var spans;
+var typed;
+var seconds = 60;
 
 const apiURL = "https://random-word-api.herokuapp.com/word?number=200&swear=1";
 
-function fetchWords() {
-    return fetch(apiURL)
-        .then( response => response.json())
+/** @returns {Promise<string[]>} */
+async function fetchWords() {
+    const res = await fetch(apiURL)
+    return await res.json()
 }
 
-async function getWords(){
-    const messages = await response.json().parse();
-    var wordsArray = [];
-    for (var i in messages){
-        wordsArray.push(messages[i]);
-    }
-}
-
-function timer(){
+function timer() {
     points = 0;
-    var timer = setInterval(function(){
+    var timer = setInterval(function () {
         button.disabled = true;
         seconds--;
         temp.innerHTML = seconds;
@@ -38,7 +30,7 @@ function timer(){
             clearInterval(timer);
             seconds = 60;
             timerDiv.innerHTML = "60";
-            button.disabled = false;	
+            button.disabled = false;
         }
     }, 1000);
 }
@@ -56,8 +48,7 @@ function wordSpitter() {
     spans = document.querySelectorAll(".span");
 }
 
-button.addEventListener("click", function(buttonClickEvent) {
-    buttonClickEvent.preventDefault();
-    fetchWords();
-    getWords();
-  });
+levelOneBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    fetchWords().then(words => console.log(words))
+});
