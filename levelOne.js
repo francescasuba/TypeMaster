fe641188a23c8c3b578fc20440ba12f33144c10e
Francesca Suba <subaap@mail.uc.edu>
2021-11-29 19:26:30 -0500

    implemented local storage high scores

--- levelOne.js
+++ levelOne.js
@@ -7,6 +7,47 @@ var points = 0;
 var seconds = 60;
 var levelOneList = [];
 const wordInputElement = document.getElementById("wordInput")
+const NO_OF_HIGH_SCORES = 5;
+const HIGH_SCORES = 'highScores';
+const highScoreString = localStorage.getItem(HIGH_SCORES);
+const highScores = JSON.parse(highScoreString) ?? [];
+const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
+
+function saveHighScore(points, highScores) {
+    const name = prompt('You got a highscore! Enter name:');
+    const newScore = { points, name };
+    
+    // 1. Add to list
+    highScores.push(newScore);
+  
+    // 2. Sort the list
+    highScores.sort((a, b) => b.points - a.points);
+    
+    // 3. Select new list
+    highScores.splice(NO_OF_HIGH_SCORES);
+    
+    // 4. Save to local storage
+    localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
+};
+
+function showHighScores() {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const highScoreList = document.getElementById(HIGH_SCORES);
+    
+    highScoreList.innerHTML = highScores
+      .map((points) => `<li>${points.points} - ${points.name}`)
+      .join('');
+}
+
+function checkHighScore(points) {
+    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.points ?? 0;
+    
+    if (points > lowestScore) {
+      saveHighScore(points, highScores); // TODO
+      showHighScores(); // TODO
+    }
+}
 
 const apiURL = "https://random-word-api.herokuapp.com/word?number=1000&swear=1";
 
@@ -43,6 +84,7 @@ function timer() {
         temp.innerHTML = seconds;
         if (seconds === 0) {
             alert("Game over! Your score is " + points);
+            checkHighScore(points);
             scoreDiv.innerHTML = "0";
             words.innerHTML = "";
             levelOneStartBtn.disabled = false;
