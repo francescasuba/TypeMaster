147d877d91f0801c1f5ef5dec882ec70ab52644f
Francesca Suba <subaap@mail.uc.edu>
2021-11-30 02:20:31 -0500

    debugged high scores feature

--- levelOne.js
+++ levelOne.js
@@ -7,13 +7,36 @@ var points = 0;
 var seconds = 60;
 var levelOneList = [];
 const wordInputElement = document.getElementById("wordInput")
-const NO_OF_HIGH_SCORES = 5;
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
+const highScoresList = document.getElementById('highScoresList');
+
+function saveHighScore() {
+    const score = {
+        score: points,
+        name: prompt('You got a highscore! Enter name:')
+    };
+    highScores.push(score);
+    highScores.sort((a,b) => b.score - a.score)
+    highScores.splice(5);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+}
+
+function displayHighScores(){
+    highScoresList.innerHTML = highScores
+        .map( score => {
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
+        })
+        .join("");
+}
+/* const NO_OF_HIGH_SCORES = 5;
 const HIGH_SCORES = 'highScores';
 const highScoreString = localStorage.getItem(HIGH_SCORES);
 const highScores = JSON.parse(highScoreString) ?? [];
 const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
-
-function saveHighScore(points, highScores) {
+ */
+/* function saveHighScore(points, highScores) {
     const name = prompt('You got a highscore! Enter name:');
     const newScore = { points, name };
     
@@ -28,26 +51,36 @@ function saveHighScore(points, highScores) {
     
     // 4. Save to local storage
     localStorage.setItem(HIGH_SCORES, JSON.stringify(highScores));
-};
+}; */
 
-function showHighScores() {
-    const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+/* function showHighScores() {
     const highScoreList = document.getElementById(HIGH_SCORES);
+    // const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
+    var displayList = document.querySelector('HIGH_SCORES');
+    displayList.innerHTML = "";
+    JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [].forEach(points => {
+        displayList.innerHTML =
+        `<ul>
+            <li>${points.points} - ${points.name}</li>
+        </ul>`
+        
+    });
+    console.log(localStorage.getItem(points));
     
     highScoreList.innerHTML = highScores
       .map((points) => `<li>${points.points} - ${points.name}`)
       .join('');
-}
+} */
 
-function checkHighScore(points) {
+/* function checkHighScore(points) {
     const highScores = JSON.parse(localStorage.getItem(HIGH_SCORES)) ?? [];
     const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.points ?? 0;
     
     if (points > lowestScore) {
-      saveHighScore(points, highScores); // TODO
-      showHighScores(); // TODO
+      saveHighScore(points, highScores);
+      showHighScores();
     }
-}
+} */
 
 const apiURL = "https://random-word-api.herokuapp.com/word?number=1000&swear=1";
 
@@ -84,7 +117,7 @@ function timer() {
         temp.innerHTML = seconds;
         if (seconds === 0) {
             alert("Game over! Your score is " + points);
-            checkHighScore(points);
+            // checkHighScore(points);
             scoreDiv.innerHTML = "0";
             words.innerHTML = "";
             levelOneStartBtn.disabled = false;
@@ -99,6 +132,7 @@ function timer() {
 
 levelOneStartBtn.addEventListener("click", function (event) {
     event.preventDefault();
+    displayHighScores();
     timer();
     fetchWords().then(words => console.log(words))
     levelOneWordSpitter();
